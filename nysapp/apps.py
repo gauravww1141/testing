11ebee55e00c1764299fbec069dfeb9cec4e1c1a
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-07-27 16:24:32 +0530

    add new data

--- nysapp/apps.py
+++ nysapp/apps.py
@@ -5,7 +5,4 @@ class NysappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'nysapp'
 
-    def ready(self):
-        # import signal handlers
-        import nysapp.signals
-
+ 
